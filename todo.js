c22707a30bbf4f0c6a15088dcf9145379811f745
antzash <antony.hashwin@gmail.com>
2024-02-21 20:43:22 +0800

    commit main

--- todo.js
+++ todo.js
@@ -7,5 +7,25 @@ flipping animation when word is revealed
 zoom in function when letter is typed in
 add a 'play again' button
 how do i center the tiles?
+make the answer a pop-up instead
+create the keyboard area
+*/
+
+/* Questions to ask 
+
+- I cant seem to center the gameboard properly, any other ways I can do so
+- adding a border bottom to the modal-css but cant change the length?
+
+
+
+
+*/
+
+/* 21 Feb goals for today 
+
+- Change the answer to a pop-up with answer
+- Random word API
+- play again button in the pop-up
+
 
 */
